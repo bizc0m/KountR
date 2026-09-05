de7af69d3ff15b82552f95962ab420b347987e94
@Bizcom <arnaud.velten@gmail.com>
2026-09-05 07:51:21 +0200

    Publish TallyCounter V2

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="app-shell">
+      <iframe
+        className="app-frame"
+        src="/TallyCounter.html"
+        title="TallyCounter"
+      />
+    </main>
+  );
+}
